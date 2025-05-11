2bdb9eac41e20ff9338c8fd29abb1085e0a21f71
Rinsutoringu <Chord_sky@outlook.com>
2025-05-11 19:14:38 +0800

    create client.h

---
+++ inc/client.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include <winsock2.h>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <thread>
+#include <mutex>
+
+#define XOR_KEY 0xAB
+
+class ChatClient {
+public:
+    /**
+     * 构造函数
+     * @param server_name 服务器名称
+     * @param port 服务器端口
+     */
+    ChatClient(const char* server_name, unsigned short port);
+    
+    /**
+     * 入口函数，启动客户端
+     */
+    void start();
+
+private:
+    // 连接的socket
+    SOCKET connect_sock;
+    // socket地址结构体
+    struct sockaddr_in server_addr;
+    // Winsock数据结构
+    WSADATA wsaData;
+    // 输出流锁
+    mutex cout_mutex;
+    // 用户名
+    string username;
+
+    /**
+     * XOR加密
+     * @param data 要加密的数据
+     * @return 加密后的数据
+     */
+    string xorCipher(const string& data);
+
+    /**
+     * 发送消息
+     * @param message 要发送的消息
+     */
+    void sendMessage(const string& message);
+
+    /**
+     * 接收消息
+     */
+    void receiveMessage();
+
+    /**
+     * 处理连接
+     */
+    void handleConnection();
+
+    /**
+     * 显示帮助
+     */
+    void showHelp();
+};
